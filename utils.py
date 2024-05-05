91a6fd3208587c02fa498baf657d97194b62b0c5
advaitkumar3107 <advaitkumar3107@gmail.com>
2024-05-05 21:38:41 +0530

    Resolved get_competitors nan handling

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def get_competitors(sheet_name, num_neighbours, ticker):
     df = pd.read_csv(sheet_name)
     x = df[(df.Symbol == ticker)]
     temp = df[df.Industry == x.Industry.iloc[0]]
-    temp['Market Cap'] = temp['Market Cap'].astype(float).astype(int)
-    temp['cap_diff'] = np.abs(temp['Market Cap'] - int(float(x['Market Cap'].iloc[0])))
+    temp['Market Cap'] = temp['Market Cap'].astype(float)
+    temp['cap_diff'] = np.abs(temp['Market Cap'] - float(x['Market Cap'].iloc[0]))
     temp = temp.sort_values('cap_diff').Symbol.head(num_neighbours).tolist()
     return temp
